a316912ee13461e50775d61b60b33574bddd9520
Charles Buczel <charles.buczel@gmail.com>
2017-06-01 00:42:00 +0100

    Week 2 Exercise 4

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
-angular.module('confusionApp', []).controller('MenuController', [ '$scope', function($scope) {
+angular.module('confusionApp', [])
+
+.controller('MenuController', [ '$scope', function($scope) {
   $scope.tab = 1;
   $scope.filtText = '';
 
@@ -70,5 +72,54 @@ angular.module('confusionApp', []).controller('MenuController', [ '$scope', func
 
   $scope.toggleDetails = function () {
     $scope.showDetails = !$scope.showDetails;
-  }
+  };
+}])
+
+.controller('ContactController', ['$scope', function($scope) {
+  $scope.feedback = {
+    mychannel: "",
+    firstName: "",
+    lastName: "",
+    agree: false,
+    email: ""
+  };
+
+  $scope.channels = [
+    {
+      value: "tel",
+      label: "Tel."
+    },
+    {
+      value: "Email",
+      label: "Email"
+    }
+  ];
+
+  $scope.invalidChannelSelection = false;
+}])
+
+.controller('FeedbackController', ['$scope', function($scope) {
+  $scope.sendFeedback = function() {
+    console.log($scope.feedback);
+
+    if ($scope.feedback.agree && $scope.feedback.mychannel === "" && !$scope.feedback.mychannel) {
+      $scope.invalidChannelSelection = true;
+
+      console.log('incorrect');
+    } else {
+      $scope.invalidChannelSelection = false;
+      $scope.feedback = {
+        mychannel: "",
+        firstName: "",
+        lastName:"",
+        agree:false,
+        email: ""
+      };
+      $scope.feedback.mychannel= "";
+      $scope.feedbackForm.$setPristine();
+
+      console.log($scope.feedback);
+    }
+  };
 }]);
+;
